8bdadd8cbfd4ba72e3847f6cd9955be356a35c7c
Ricardo <ej.ricardogomes@gmail.com>
2022-12-31 06:39:57 -0300

    Adição de teste unitário para quando não encontrar conteúdo no caso de uso.

--- Tests/Features/GetUserById/GetUserByIdUseCaseTest.cs
+++ Tests/Features/GetUserById/GetUserByIdUseCaseTest.cs
@@ -36,5 +36,21 @@ namespace Tests.Features.GetUserById
             Assert.True(call.Result.IsValid);
             Assert.Null(call.Result.Errors);
         }
-    }
+
+
+        [Fact]
+        public void GetUserNotReturnResult()
+        {
+            var userResult = new List<User>();
+
+            _mock.Setup(x => x.GetUserByIdAsync(19)).ReturnsAsync(userResult);
+
+            var useCase = new GetUserByIdUseCase(_mock.Object);
+            var call = useCase.ExecuteAsync(19);
+            Assert.Null(call.Result.Data);
+            Assert.False(call.Result.IsValid);
+            Assert.NotNull(call.Result.Errors);
+        }
+    
+}
 }
